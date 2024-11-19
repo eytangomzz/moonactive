07f85bc5a2a95d1bb1b5e4ec1ac2571c02b67b75
eytan gomez <eytan.gomez@infinitylabs.co.il>
2024-11-19 11:22:39 +0200

    added app with odd number and init setup

---
+++ app/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, make_response
+import random
+import os
+
+app = Flask(__name__)
+
+app_ready = False
+
+
+## Create the log file on startup
+
+def initialize_app():
+    global app_ready
+    file_path = os.path.join(os.path.dirname(__file__), 'odd-logs.txt')
+    if not os.path.exists(file_path):
+        open(file_path, 'w').close()
+    app_ready = True
+
+## Odd function
+@app.route('/odd', methods=['GET'])
+def get_random_odd():
+    odd_number = [n for n in range(1, 20) if n % 2 != 0]
+    random_number = random.choice(odd_number)
+    log_file_path = os.path.join(os.path.dirname(__file__), 'odd-logs.txt')
+    with open(log_file_path, 'a') as log_file:
+        log_file.write(f"Odd number: {random_number}\n")
+    return jsonify({"Odd number": random_number})
+
+## Is app ready
+@app.route('/ready', methods=['GET'])
+def readiness_check():
+    if app_ready:
+        return make_response("Application is ready", 200)
+    else:
+        return make_response("Application is not ready", 503)
+
+## Dont forget to remove debug=True after app is finished
+if __name__ == '__main__':
+    initialize_app()
+    app.run(host='0.0.0.0', port=5000, debug=True)
