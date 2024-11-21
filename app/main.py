019221a0f349772d03193907e361e99f35c4d3cf
eytan gomez <eytan.gomez@infinitylabs.co.il>
2024-11-21 12:03:36 +0200

    fixed some pylint errors

--- app/main.py
+++ app/main.py
@@ -1,39 +1,44 @@
+"""
+Simple Flask application for generating random odd numbers.
+"""
+
 import random
 import os
 from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
-APP_READY = False
 
-
-## Create the log file on startup
 
 def initialize_app():
-    global APP_READY
+    """ Initialize the app and create log file."""
     file_path = os.path.join(os.path.dirname(__file__), 'odd-logs.txt')
     if not os.path.exists(file_path):
-        open(file_path, 'w').close()
-    APP_READY = True
+        with open(file_path, 'w'):
+            pass
+    return True
 
-## Odd function
+
 @app.route('/odd', methods=['GET'])
 def get_random_odd():
+    """ Generate odd number and write it to a log file."""
     odd_number = [n for n in range(1, 20) if n % 2 != 0]
     random_number = random.choice(odd_number)
     log_file_path = os.path.join(os.path.dirname(__file__), 'odd-logs.txt')
-    with open(log_file_path, 'a') as log_file:
+    with open(log_file_path, 'a', encoding='utf-8') as log_file:
         log_file.write(f"Odd number: {random_number}\n")
     return jsonify({"Odd number": random_number})
 
-## Is app ready
+
 @app.route('/ready', methods=['GET'])
 def readiness_check():
+    """ Check if the app is ready to get requests."""
     if APP_READY:
         return make_response("Application is ready", 200)
     return make_response("Application is not ready", 503)
 
+
 ## Dont forget to remove debug=True after app is finished
 if __name__ == '__main__':
-    initialize_app()
+    APP_READY = initialize_app()
     app.run(host='0.0.0.0', port=5000, debug=True)
