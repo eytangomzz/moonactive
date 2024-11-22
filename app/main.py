e542df8ff233443ad812add23632e47a926818d0
eytan gomez <eytan.gomez@infinitylabs.co.il>
2024-11-22 15:32:30 +0200

    quickfix app volume

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ APP_READY = False
 
 def initialize_app():
     """ Initialize the app and create log file."""
-    file_path = os.path.join(os.path.dirname(__file__), 'odd-logs.txt')
+    file_path = os.path.join(os.path.dirname(__file__), '/logs/odd-logs.txt')
     if not os.path.exists(file_path):
         with open(file_path, 'w', encoding='utf-8'):
             pass
@@ -23,7 +23,7 @@ def get_random_odd():
     """ Generate odd number and write it to a log file."""
     odd_number = [n for n in range(1, 20) if n % 2 != 0]
     random_number = random.choice(odd_number)
-    log_file_path = os.path.join(os.path.dirname(__file__), 'odd-logs.txt')
+    log_file_path = os.path.join(os.path.dirname(__file__), '/logs/odd-logs.txt')
     with open(log_file_path, 'a', encoding='utf-8') as log_file:
         log_file.write(f"Odd number: {random_number}\n")
     return jsonify({"Odd number": random_number})
