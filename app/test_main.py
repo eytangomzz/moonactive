dbde8275333a58b34480146d8af2e50c0868f794
eytan gomez <eytan.gomez@infinitylabs.co.il>
2024-11-23 08:25:45 +0200

    added timeout and dockstring

--- app/test_main.py
+++ app/test_main.py
@@ -1,10 +1,14 @@
+"""
+Test for the app
+"""
+
 import requests
 
 BASE_URL = "http://localhost:5000"
 
 def test_ready_endpoint():
     """Test the readiness endpoint."""
-    response = requests.get(f"{BASE_URL}/ready")
+    response = requests.get(f"{BASE_URL}/ready", timeout=5)
     assert response.status_code in [200, 503], "Unexpected status code for /ready"
     if response.status_code == 200:
         assert response.text == "Application is ready"
@@ -12,7 +16,7 @@ def test_ready_endpoint():
 
 def test_odd_endpoint():
     """Test the odd number generation endpoint."""
-    response = requests.get(f"{BASE_URL}/odd")
+    response = requests.get(f"{BASE_URL}/odd", timeout=5)
     assert response.status_code == 200, "Unexpected status code for /odd"
     data = response.json()
     assert isinstance(data["Odd number"], int), "'Odd number' is not an integer"
